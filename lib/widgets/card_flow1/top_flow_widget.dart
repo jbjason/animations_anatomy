4998a3610309f190b45ac78cf4a42c0770993c04
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-01 22:31:16 +0600

    s

--- lib/widgets/card_flow1/top_flow_widget.dart
+++ lib/widgets/card_flow1/top_flow_widget.dart
@@ -8,6 +8,24 @@ class TopFlowWidget extends StatelessWidget {
   final EdgeInsets padding;
   @override
   Widget build(BuildContext context) {
+    final _title = TextStyle(
+        color:
+            Colors.black.computeLuminance() < .5 ? Colors.white : Colors.black,
+        fontSize: 22,
+        fontWeight: FontWeight.bold);
+    final _price = TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color:
+            Colors.black.computeLuminance() < .5 ? Colors.white : Colors.black);
+    final _place = TextStyle(
+        fontSize: 13,
+        color:
+            Colors.black.computeLuminance() < .5 ? Colors.white : Colors.black);
+    final _date = TextStyle(
+        fontSize: 11,
+        color:
+            Colors.black.computeLuminance() < .5 ? Colors.white : Colors.black);
     return Hero(
       tag: 'top ${cardItem.title}',
       child: Stack(
@@ -19,55 +37,23 @@ class TopFlowWidget extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  cardItem.title,
-                  style: TextStyle(
-                      color: Colors.black.computeLuminance() < .5
-                          ? Colors.white
-                          : Colors.black,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  cardItem.price,
-                  style: TextStyle(
-                      color: Colors.black.computeLuminance() < .5
-                          ? Colors.white
-                          : Colors.black,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                ),
+                Text(cardItem.title, style: _title),
+                Text(cardItem.price, style: _price),
                 const SizedBox(height: 20),
-                Text(
-                  cardItem.place,
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.black.computeLuminance() < .5
-                        ? Colors.white
-                        : Colors.black,
-                  ),
-                ),
+                Text(cardItem.place, style: _place),
                 Row(
                   children: [
-                    Text(
-                      cardItem.date,
-                      style: TextStyle(
-                        fontSize: 11,
-                        color: Colors.black.computeLuminance() < .5
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                    ),
+                    Text(cardItem.date, style: _date),
                     const Spacer(),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white.withOpacity(0.4)),
-                      child: const Text('CheckIn',
-                          overflow: TextOverflow.ellipsis),
-                    ),
-                    const SizedBox(width: 10),
+                    // Container(
+                    //   padding: const EdgeInsets.all(10),
+                    //   decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.circular(20),
+                    //       color: Colors.white.withOpacity(0.4)),
+                    //   child: const Text('CheckIn',
+                    //       overflow: TextOverflow.ellipsis),
+                    // ),
+                    // const SizedBox(width: 10),
                   ],
                 ),
               ],

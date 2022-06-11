863795aa199ccba805d1fddcd2a758733d3e5887
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-11 14:00:17 +0600

    Update book_screen.dart

--- lib/screens/book_screen.dart
+++ lib/screens/book_screen.dart
@@ -31,49 +31,44 @@ class _BookScreenState extends State<BookScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[300],
       body: Stack(
         children: [
           Positioned.fill(
-              child: Image.asset(
-            'assets/card_/background.png',
-            fit: BoxFit.cover,
-          )),
-          Positioned(
-            child: AppBar(
-              leading: InkWell(
-                  onTap: () => Navigator.pop(context),
-                  child: const Icon(Icons.arrow_back_ios_new)),
-              title: const Text('Book Concept'),
-            ),
+            child:
+                Image.asset('assets/card_/background.png', fit: BoxFit.cover),
           ),
+          // Positioned(
+          //   child: AppBar(
+          //     leading: InkWell(
+          //         onTap: () => Navigator.pop(context),
+          //         child: const Icon(Icons.arrow_back_ios_new)),
+          //     title: const Text('Book Concept'),
+          //   ),
+          // ),
           Positioned(
             top: kToolbarHeight + 30,
-            height: size.height * .8,
-            width: size.width,
-            child: Container(
-              color: Colors.white,
-              child: ValueListenableBuilder<double>(
-                  valueListenable: _notifier,
-                  builder: (context, double value, _) {
-                    return PageView.builder(
-                      controller: _controller,
-                      itemBuilder: (ctx, index) {
-                        // 0.9 ,o.8 to 0.1, 0.0
-                        final percent = (index - value).clamp(0.0, 1.0);
-                        print(percent);
-                        return Transform(
-                            alignment: Alignment.centerLeft,
-                            transform: Matrix4.identity()
-                              ..setEntry(3, 2, 002)
-                              ..rotateY(2 * percent)
-                              ..scale(1 + percent)
-                              ..translate(-size.width * .8 * percent),
-                            child: BookItem(book: books[index]));
-                      },
-                    );
-                  }),
-            ),
+            left: 0,
+            right: 0,
+            child: ValueListenableBuilder<double>(
+                valueListenable: _notifier,
+                builder: (context, double value, _) {
+                  return PageView.builder(
+                    controller: _controller,
+                    itemBuilder: (ctx, index) {
+                      // 0.9 ,o.8 to 0.1, 0.0
+                      final percent = (index - value).clamp(0.0, 1.0);
+                      return Transform(
+                          alignment: Alignment.centerLeft,
+                          transform: Matrix4.identity()
+                            ..setEntry(3, 2, 002)
+                            ..rotateY(2 * percent)
+                            ..scale(1 + percent)
+                            ..translate(-size.width * .8 * percent),
+                          child: BookItem(book: books[index]));
+                    },
+                  );
+                }),
           ),
         ],
       ),
@@ -86,20 +81,17 @@ class BookItem extends StatelessWidget {
   final Book book;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            book.image,
-            fit: BoxFit.cover,
-            width: MediaQuery.of(context).size.width * .8,
-          ),
-          const SizedBox(height: 20),
-          Text(book.title),
-          Text(book.author),
-        ],
-      ),
+    return Column(
+      children: [
+        Image.asset(
+          book.image,
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width * .8,
+        ),
+        const SizedBox(height: 20),
+        Text(book.title),
+        Text(book.author),
+      ],
     );
   }
 }

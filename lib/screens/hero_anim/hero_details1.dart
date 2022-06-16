8117eccaeb942a2414ee2f5e632a68f1d42aa023
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-16 08:57:12 +0600

    s

---
+++ lib/screens/hero_anim/hero_details1.dart
@@ -0,0 +1,76 @@
+import 'package:animations_anatomy/models/travel.dart';
+import 'package:flutter/material.dart';
+import 'package:animations_anatomy/constants/constants.dart';
+
+class HeroDetails1 extends StatefulWidget {
+  const HeroDetails1({Key? key, required this.travel}) : super(key: key);
+  final Travel travel;
+  @override
+  State<HeroDetails1> createState() => _HeroDetails1State();
+}
+
+class _HeroDetails1State extends State<HeroDetails1>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    _animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutBack,
+        reverseCurve: Curves.easeInOutBack));
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(Icons.arrow_back_ios_new)),
+              Hero(
+                tag: widget.travel.imageFront + widget.travel.imageBack,
+                child: Transform(
+                    origin: const Offset(-75, -150),
+                    transform: Matrix4.identity()
+                      ..rotateZ(2 * _animation.value),
+                    alignment: Alignment.topLeft,
+                    child: Image.asset(widget.travel.imageFront,
+                        fit: BoxFit.cover)),
+              ),
+              const SizedBox(height: 40),
+              Center(
+                child: Text(
+                  widget.travel.title,
+                  style: const TextStyle(
+                      fontSize: 23, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                '$detailsText\n$detailsText',
+                style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/hero_challange1.dart
+++ lib/screens/hero_challange1.dart
@@ -1,5 +1,5 @@
-import 'package:animations_anatomy/constants/constants.dart';
 import 'package:animations_anatomy/models/travel.dart';
+import 'package:animations_anatomy/screens/hero_anim/hero_details1.dart';
 import 'package:flutter/material.dart';
 
 class HeroChallenge1 extends StatefulWidget {
@@ -8,23 +8,18 @@ class HeroChallenge1 extends StatefulWidget {
   State<HeroChallenge1> createState() => _HeroChallenge1State();
 }
 
-class _HeroChallenge1State extends State<HeroChallenge1>
-    with SingleTickerProviderStateMixin {
+class _HeroChallenge1State extends State<HeroChallenge1> {
   late PageController _pageController;
-  late AnimationController _controller;
 
   @override
   void initState() {
     _pageController = PageController(viewportFraction: 0.9, initialPage: 0);
-    _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 500));
     super.initState();
   }
 
   @override
   void dispose() {
     _pageController.dispose();
-    _controller.dispose();
     super.dispose();
   }
 
@@ -36,7 +31,7 @@ class _HeroChallenge1State extends State<HeroChallenge1>
           child: PageView.builder(
             controller: _pageController,
             itemBuilder: (context, index) {
-              return HeroItem(travel: travels[index], controller: _controller);
+              return HeroItem(travel: travels[index]);
             },
             itemCount: travels.length,
           ),
@@ -47,20 +42,21 @@ class _HeroChallenge1State extends State<HeroChallenge1>
 }
 
 class HeroItem extends StatelessWidget {
-  const HeroItem({Key? key, required this.travel, required this.controller})
-      : super(key: key);
+  const HeroItem({Key? key, required this.travel}) : super(key: key);
   final Travel travel;
-  final AnimationController controller;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         InkWell(
           onTap: () async {
-            await Navigator.of(context).push(PageRouteBuilder(
-                pageBuilder: ((context, animation, secondaryAnimation) {
-              return HeroDetails1(travel: travel);
-            })));
+            await Navigator.of(context).push(
+              PageRouteBuilder(
+                  reverseTransitionDuration: const Duration(milliseconds: 600),
+                  pageBuilder: ((context, animation, secondaryAnimation) {
+                    return HeroDetails1(travel: travel);
+                  })),
+            );
           },
           child: Hero(
             tag: travel.imageFront + travel.imageBack,
@@ -74,46 +70,6 @@ class HeroItem extends StatelessWidget {
           style: const TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
         )
       ],
-    );
-  }
-}
-
-class HeroDetails1 extends StatelessWidget {
-  const HeroDetails1({Key? key, required this.travel}) : super(key: key);
-  final Travel travel;
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: const Icon(Icons.arrow_back_ios_new)),
-              Hero(
-                tag: travel.imageFront + travel.imageBack,
-                child: Image.asset(travel.imageFront, fit: BoxFit.cover),
-              ),
-              const SizedBox(height: 40),
-              Center(
-                child: Text(
-                  travel.title,
-                  style: const TextStyle(
-                      fontSize: 23, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 40),
-              const Text(
-                '$detailsText\n$detailsText',
-                style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }

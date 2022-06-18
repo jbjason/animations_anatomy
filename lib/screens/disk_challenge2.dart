48fae4334c16cc585f333f053e11a6ea92c2d8f7
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-19 05:06:00 +0600

    s

--- lib/screens/disk_challenge2.dart
+++ lib/screens/disk_challenge2.dart
@@ -12,6 +12,7 @@ class _DiskChallenge2State extends State<DiskChallenge2>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   bool _isGrid = false;
+  int _count = 2;
 
   @override
   void initState() {
@@ -29,6 +30,7 @@ class _DiskChallenge2State extends State<DiskChallenge2>
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[800],
@@ -42,7 +44,10 @@ class _DiskChallenge2State extends State<DiskChallenge2>
                 _controller.status == AnimationStatus.completed
                     ? await _controller.reverse()
                     : await _controller.forward();
-                setState(() => _isGrid = !_isGrid);
+                setState(() {
+                  _isGrid = !_isGrid;
+                  _count = _count != 2 ? 2 : 1;
+                });
               },
               child: _isGrid
                   ? const Icon(Icons.grid_4x4_sharp, color: Colors.white)
@@ -53,10 +58,14 @@ class _DiskChallenge2State extends State<DiskChallenge2>
         ),
         body: Padding(
           padding: const EdgeInsets.all(10),
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return DiskItem(trip: trips[index], controller: _controller);
-            },
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: _count,
+                mainAxisSpacing: 5,
+                mainAxisExtent: size.height * .16,
+                crossAxisSpacing: 10),
+            itemBuilder: (context, index) =>
+                DiskItem(trip: trips[index], controller: _controller),
             itemCount: trips.length,
           ),
         ),
@@ -73,31 +82,25 @@ class DiskItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      padding: const EdgeInsets.only(bottom: 15),
-      height: size.height * .18,
-      width: size.width,
-      child: Stack(
-        children: [
-          AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) => Positioned(
-              left: 125 * controller.value,
-              height: size.height * .18 - 10,
-              child: Transform.rotate(
-                  alignment: Alignment.center,
-                  angle: vector.radians(360 * controller.value),
-                  child:
-                      Image.asset('assets/trip/disk_.png', fit: BoxFit.cover)),
-            ),
+    return Stack(
+      children: [
+        AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) => Positioned(
+            left: 60 * controller.value,
+            bottom: 5,
+            child: Transform.rotate(
+                alignment: Alignment.center,
+                angle: vector.radians(360 * controller.value),
+                child: Image.asset('assets/trip/disk_.png',
+                    height: size.height * .16, fit: BoxFit.cover)),
           ),
-          Positioned(
-            left: 0,
-            width: 150,
-            child: Image.asset(trip.img, fit: BoxFit.cover),
-          ),
-        ],
-      ),
+        ),
+        Positioned(
+          left: 0,
+          child: Image.asset(trip.img, fit: BoxFit.fitHeight, width: 120),
+        ),
+      ],
     );
   }
 }

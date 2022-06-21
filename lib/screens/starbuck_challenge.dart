bb5dc1949e4076df1e22d68dc6aa91ca2c46ae78
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-21 17:31:20 +0600

    Update starbuck_challenge.dart

--- lib/screens/starbuck_challenge.dart
+++ lib/screens/starbuck_challenge.dart
@@ -13,7 +13,7 @@ class _StarbuckChallengeState extends State<StarbuckChallenge> {
 
   @override
   void initState() {
-    _controller = PageController(initialPage: 0, viewportFraction: 0.9);
+    _controller = PageController(initialPage: 0, viewportFraction: 0.85);
     _controller.addListener(_listen);
     super.initState();
   }
@@ -29,6 +29,7 @@ class _StarbuckChallengeState extends State<StarbuckChallenge> {
 
   @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         body: Column(
@@ -39,23 +40,20 @@ class _StarbuckChallengeState extends State<StarbuckChallenge> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Image.asset('assets/starbuck/location.png', height: 20),
+                  Image.asset('assets/starbuck/location.png', height: 30),
                   Image.asset('assets/starbuck/logo.png'),
-                  Image.asset('assets/starbuck/drawer.png', height: 20),
+                  Image.asset('assets/starbuck/drawer.png', height: 30),
                 ],
               ),
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: PageView.builder(
-                  controller: _controller,
-                  itemBuilder: (context, index) {
-                    final _percent = (index - _value).abs();
-                    return DrinkItem(drink: drinkList[index]);
-                  },
-                  itemCount: drinkList.length,
-                ),
+              child: PageView.builder(
+                controller: _controller,
+                itemBuilder: (context, index) {
+                  final _percent = (index - _value).abs();
+                  return DrinkItem(drink: drinkList[index], size: _size);
+                },
+                itemCount: drinkList.length,
               ),
             ),
           ],
@@ -66,118 +64,153 @@ class _StarbuckChallengeState extends State<StarbuckChallenge> {
 }
 
 class DrinkItem extends StatelessWidget {
-  const DrinkItem({Key? key, required this.drink}) : super(key: key);
+  const DrinkItem({Key? key, required this.size, required this.drink})
+      : super(key: key);
   final Drink drink;
+  final Size size;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         _title(),
-        const SizedBox(height: 60),
+        const SizedBox(height: 30),
         Expanded(
           child: Container(
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.only(right: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                  image: AssetImage(drink.backgroundImage), fit: BoxFit.cover),
-            ),
-            child: Stack(
-              children: [
-                _column(),
-              ],
-            ),
-          ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: AssetImage(drink.backgroundImage),
+                    fit: BoxFit.cover),
+              ),
+              child: _containerBody()),
         ),
         const SizedBox(height: 70),
       ],
     );
   }
 
-  Widget _title() => Row(
+  Widget _containerBody() => Stack(
         children: [
-          Text(
-            drink.name,
-            style: TextStyle(
-                color: drink.lightColor,
-                fontSize: 34,
-                fontWeight: FontWeight.bold),
+          _column(),
+          Positioned(
+            right: -20,
+            bottom: -50,
+            child: Image.asset(drink.cupImage, height: size.height * .6),
           ),
-          Text(
-            drink.conName,
-            style: TextStyle(
-                color: drink.darkColor,
-                fontSize: 35,
-                fontWeight: FontWeight.bold),
+          Positioned(
+            left: size.width * 0.15,
+            top: -30,
+            child: Image.asset(drink.imageTop, height: 80, width: 80),
+          ),
+          Positioned(
+            right: -10,
+            top: size.height * .15,
+            child: Image.asset(
+              drink.imageSmall,
+              height: 40,
+              width: 40,
+            ),
+          ),
+          Positioned(
+            bottom: -10,
+            left: size.width * .2,
+            child: Image.asset(drink.imageBlur, height: 100, width: 100),
           ),
         ],
       );
 
-  Widget _column() => Column(
-        children: [
-          const Text(
-            'Frappuccino',
-            style: TextStyle(
-                color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 15),
-          Text(
-            drink.description,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 5,
-            style: TextStyle(color: Colors.white.withOpacity(0.3)),
-          ),
-          const Spacer(),
-          Container(
-            height: 60,
-            padding: const EdgeInsets.only(left: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const SizedBox(width: 20),
-                Image.asset('assets/starbuck/cup_L.png', height: 40),
-                const SizedBox(width: 7),
-                Image.asset('assets/starbuck/cup_M.png', height: 30),
-                const SizedBox(width: 7),
-                Image.asset('assets/starbuck/cup_s.png', height: 20),
-              ],
+  Widget _title() => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Text(
+              drink.name,
+              style: TextStyle(
+                  color: drink.lightColor,
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-          const SizedBox(height: 15),
-          Container(
-            height: 60,
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              gradient: LinearGradient(
-                colors: [mAppGreen, mToGreen],
-                stops: const [0.2, 1.0],
-              ),
+            Text(
+              drink.conName,
+              style: TextStyle(
+                  color: drink.darkColor,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
             ),
-            child: RichText(
+          ],
+        ),
+      );
+
+  Widget _column() => Container(
+        margin: const EdgeInsets.only(right: 20),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const Text(
+              'Frappuccino',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              drink.description,
               overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: '\$ ',
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 27),
+              maxLines: 5,
+              style: TextStyle(color: Colors.white.withOpacity(0.3)),
+            ),
+            const Spacer(),
+            Container(
+              height: 60,
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  TextSpan(
-                    text: '${drink.price.toInt()}.',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  TextSpan(
-                    text: drink.price.toString().split('.')[1],
-                    style: const TextStyle(fontSize: 13),
-                  ),
+                  const SizedBox(width: 20),
+                  Image.asset('assets/starbuck/cup_L.png', height: 40),
+                  const SizedBox(width: 7),
+                  Image.asset('assets/starbuck/cup_M.png', height: 30),
+                  const SizedBox(width: 7),
+                  Image.asset('assets/starbuck/cup_s.png', height: 20),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-        ],
+            const SizedBox(height: 15),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                gradient: LinearGradient(
+                  colors: [mAppGreen, mToGreen],
+                  stops: const [0.2, 1.0],
+                ),
+              ),
+              child: RichText(
+                overflow: TextOverflow.ellipsis,
+                text: TextSpan(
+                  text: '\$ ',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 27),
+                  children: [
+                    TextSpan(
+                      text: '${drink.price.toInt()}.',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    TextSpan(
+                      text: drink.price.toString().split('.')[1],
+                      style: const TextStyle(fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       );
 }

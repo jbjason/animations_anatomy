5fdf0f1f0982cef02c61d9e0a79aa15888de185d
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-06-21 14:57:48 +0600

    s

---
+++ lib/models/drink.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+Color mBrown = const Color(0xff81554E);
+Color mBrownLight = const Color(0xffC09C88);
+Color greenLight = const Color(0xffA3DB72);
+Color greenDark = const Color(0xff497E19);
+Color mAppGreen = const Color(0xff067655);
+Color mToGreen = const Color(0xff033B2B);
+
+class Drink {
+  final String name;
+  final String conName;
+  final String backgroundImage;
+  final String imageTop;
+  final String imageSmall;
+  final String imageBlur;
+  final String cupImage;
+  final String description;
+  final Color lightColor;
+  final Color darkColor;
+
+  const Drink(
+      {required this.name,
+      required this.conName,
+      required this.backgroundImage,
+      required this.imageTop,
+      required this.imageSmall,
+      required this.imageBlur,
+      required this.cupImage,
+      required this.description,
+      required this.lightColor,
+      required this.darkColor});
+}
+
+List<Drink> drinkList = [
+  Drink(
+    name: 'Tirami',
+    conName: 'Sù',
+    backgroundImage: 'images/blur_image.png',
+    imageTop: 'images/bean_top.png',
+    imageSmall: 'images/bean_small.png',
+    imageBlur: 'images/bean_blur.png',
+    cupImage: 'images/cup.png',
+    description:
+        'then top with whipped cream and mocha drizzle to bring you endless \njava joy',
+    lightColor: mAppGreen,
+    darkColor: mToGreen,
+  ),
+  Drink(
+    name: 'Green',
+    conName: 'Tea',
+    backgroundImage: 'images/green_image.png',
+    imageTop: 'images/green_top.png',
+    imageSmall: 'images/green_small.png',
+    imageBlur: 'images/green_blur.png',
+    cupImage: 'images/green_tea_cup.png',
+    description:
+        'milk and ice and top it with sweetened whipped cream to give you \na delicious boost\nof energy.',
+    lightColor: greenLight,
+    darkColor: greenDark,
+  ),
+  Drink(
+    name: 'Triple',
+    conName: 'Mocha',
+    backgroundImage: 'images/mocha_image.png',
+    imageTop: 'images/chocolate_top.png',
+    imageSmall: 'images/chocolate_small.png',
+    imageBlur: 'images/chocolate_blur.png',
+    cupImage: 'images/mocha_cup.png',
+    description:
+        'layers of whipped cream that\'s infused with cold brew, white chocolate mocha and dark \ncaramel.',
+    lightColor: mBrownLight,
+    darkColor: mBrown,
+  ),
+];
--- lib/screens/starbuck_challenge.dart
+++ lib/screens/starbuck_challenge.dart
@@ -1,70 +1,15 @@
 import 'package:flutter/material.dart';
 
-Color mBrown = const Color(0xff81554E);
-Color mBrownLight = const Color(0xffC09C88);
-Color greenLight = const Color(0xffA3DB72);
-Color greenDark = const Color(0xff497E19);
-Color mAppGreen = const Color(0xff067655);
-Color mToGreen = const Color(0xff033B2B);
+class StarbuckChallenge extends StatefulWidget {
+  const StarbuckChallenge({Key? key}) : super(key: key);
 
-class Drink {
-  String name;
-  String conName;
-  String backgroundImage;
-  String imageTop;
-  String imageSmall;
-  String imageBlur;
-  String cupImage;
-  String description;
-  Color lightColor;
-  Color darkColor;
-
-  Drink(
-    this.name,
-    this.conName,
-    this.backgroundImage,
-    this.imageTop,
-    this.imageSmall,
-    this.imageBlur,
-    this.cupImage,
-    this.description,
-    this.lightColor,
-    this.darkColor,
-  );
+  @override
+  State<StarbuckChallenge> createState() => _StarbuckChallengeState();
 }
 
-List<Drink> drinkList = [
-  Drink(
-      'Tirami',
-      'Sù',
-      'images/blur_image.png',
-      'images/bean_top.png',
-      'images/bean_small.png',
-      'images/bean_blur.png',
-      'images/cup.png',
-      'then top with whipped cream and mocha drizzle to bring you endless \njava joy',
-      mBrownLight,
-      mBrown),
-  Drink(
-      'Green',
-      'Tea',
-      'images/green_image.png',
-      'images/green_top.png',
-      'images/green_small.png',
-      'images/green_blur.png',
-      'images/green_tea_cup.png',
-      'milk and ice and top it with sweetened whipped cream to give you \na delicious boost\nof energy.',
-      greenLight,
-      greenDark),
-  Drink(
-      'Triple',
-      'Mocha',
-      'images/mocha_image.png',
-      'images/chocolate_top.png',
-      'images/chocolate_small.png',
-      'images/chocolate_blur.png',
-      'images/mocha_cup.png',
-      'layers of whipped cream that’s infused with cold brew, white chocolate mocha and dark \ncaramel.',
-      mBrownLight,
-      mBrown)
-];
+class _StarbuckChallengeState extends State<StarbuckChallenge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

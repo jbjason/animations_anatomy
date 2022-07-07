f894667bf3e12a8161a38674ca1314b001e9b1bd
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-07-07 17:48:48 +0600

    Update fb_chlng1_screen.dart

--- lib/screens/fb_chlng1_screen.dart
+++ lib/screens/fb_chlng1_screen.dart
@@ -5,6 +5,50 @@ class FbChlng1Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: FbChlngDelegate(
+              maxExtend: size.height * .4,
+              minExtend: kTextTabBarHeight + 30,
+              size: size,
+            ),
+          ),
+          const SliverToBoxAdapter(child: Placeholder()),
+          const SliverToBoxAdapter(child: Placeholder()),
+          const SliverToBoxAdapter(child: Placeholder()),
+        ],
+      ),
+    );
   }
+}
+
+class FbChlngDelegate extends SliverPersistentHeaderDelegate {
+  const FbChlngDelegate(
+      {required this.maxExtend, required this.size, required this.minExtend});
+  final double maxExtend, minExtend;
+  final Size size;
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    final double percent = (shrinkOffset / maxExtend).clamp(0, 1.0);
+    print(percent);
+    return Container(
+      color: Colors.grey[900],
+    );
+  }
+
+  @override
+  double get maxExtent => maxExtend;
+
+  @override
+  double get minExtent => minExtend;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
 }

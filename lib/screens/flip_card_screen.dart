e1bddc74b28cf523f77a48de8b59cc71959e9c9c
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-07-27 07:10:55 +0600

    Update flip_card_screen.dart

--- lib/screens/flip_card_screen.dart
+++ lib/screens/flip_card_screen.dart
@@ -20,37 +20,61 @@ class _FlipCardScreenState extends State<FlipCardScreen>
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: const Text('Testing Screen'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('FlipCard Screen'),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.g_translate))
+        ],
+      ),
       body: AnimatedBuilder(
         animation: _controller,
         builder: (context, _) {
           final _val = _controller.value;
           return Stack(
             children: [
-              Positioned(
-                top: 60,
-                left: 0,
-                right: 0,
-                child: Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()
-                    ..setEntry(3, 2, .002)
-                    ..rotateY(math.pi * _val),
-                  child: _val < 0.5
-                      ? Image.asset('assets/card_/11.jpg', fit: BoxFit.contain)
-                      : Image.asset('assets/card_/9.jpg', fit: BoxFit.contain),
-                ),
-              ),
-              Positioned(
-                bottom: 50,
-                left: 150,
-                child: ElevatedButton(
-                  onPressed: () => _val < 1
-                      ? _controller.forward(from: 0.0)
-                      : _controller.reverse(),
-                  child: const Text('TextButton'),
-                ),
+              Positioned.fill(child: Container(color: Colors.white)),
+              Stack(
+                children: [
+                  Positioned.fill(child: Container(color: Colors.blueGrey)),
+                  Positioned(
+                    top: 60,
+                    left: 0,
+                    right: 0,
+                    child: Transform.translate(
+                      offset: Offset(size.width * .7, 0),
+                      child: Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, .002)
+                          ..rotateY(-math.pi * (_val / 2)),
+                        child: Image.asset('asets/card_/7.jpg',
+                            height: 300, fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 50,
+                    left: 150,
+                    child: ElevatedButton(
+                      onPressed: () => _val < 1
+                          ? _controller.forward(from: 0.0)
+                          : _controller.reverse(),
+                      child: const Text('TextButton'),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: AppBar(
+                      title: const Text('Testing Screen'),
+                      centerTitle: true,
+                    ),
+                  )
+                ],
               ),
             ],
           );
